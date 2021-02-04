756543f6298c006eee0646a2ee2ec961c2419ccb
Isaiah Davis <ID@Isaiahs-iMac.local>
2021-02-04 12:26:53 -0500

    commit tree c197dd813bb2c5d7ab5ac81f9ed2f59fce1dcd45

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,12 @@
-def catch_phrase
+def catch_phrase(phrase = "It's-a me, Mario!")
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
+catch_phrase
+
+describe "#catch_phrase" do
+  it "puts out a catch phrase" do
+    phrase = "It's-a me, Mario!"
+    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
+  end
+end 
